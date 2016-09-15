60c3060f20101561126c338b1e95f405a8b1a3b9
Raj Prasad <raj-prasad@live.com>
2016-09-16 11:08:16 +1200

    Initialize project

---
+++ testApp/testApp/Controllers/NewController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace testApp.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View("test");
+        }
+
+        //add sstring
+        public string getString()
+        {
+            return "this is a string";
+        }
+    }
+}
