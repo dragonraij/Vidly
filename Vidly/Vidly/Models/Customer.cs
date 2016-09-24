cd60f13394377868a24106bc7e9d16431881bdf9
Raj Prasad <raj-prasad@live.com>
2016-09-24 22:19:24 +1200

    Add custom model validation on data add

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -6,14 +6,20 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewslettter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? DateOfBirth { get; set; }
     }
 }
---
+++ Vidly/Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.DateOfBirth == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - customer.DateOfBirth.Value.Year;
+
+            return (age >= 18) ? ValidationResult.Success
+                : new ValidationResult("customer should be at least 18 years old to go on a membership plan");
+        }
+    }
+}
